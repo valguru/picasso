fc2b5e8a116ebeb001bee61e9c07d275e7e673ab
valguru <val.guru@mail.ru>
2024-03-21 10:57:13 +0300

    add back button

--- src/widgets/postDetails/ui/index.tsx
+++ src/widgets/postDetails/ui/index.tsx
@@ -1,25 +1,32 @@
 import React from "react";
-import {useParams} from "react-router-dom";
-import {useGetPostByIdQuery} from "../../api";
+import {Link, useParams} from "react-router-dom";
+import {useGetPostByIdQuery} from "../../api/postApi";
+import {useDispatch} from "react-redux";
+import {setStartPost} from "../../api/postPagination";
 import "./postDetails.scss";
 
 export const PostDetails: React.FC = () => {
-    const { id } = useParams<{ id: string }>();
+    const dispatch = useDispatch();
+    const {id} = useParams<{ id: string }>();
     const {data: post, isLoading} = useGetPostByIdQuery(Number(id))
+
+    const handleGoBack = () => {
+        dispatch(setStartPost(post?.id || 0));
+    };
 
     if(isLoading) {
         return <div className="stub">Загрузка...</div>
     }
+
     if (!post) {
         return <div className="stub">Такого поста не существует</div>;
     }
 
     return (
         <div className="post_details">
-
             <h1>{`${post.id}. ${post.title}`}</h1>
             <p>{post.body}</p>
-
+            <Link to="/" className="btn" onClick={handleGoBack}>Назад</Link>
         </div>
     )
 }
